12a3a895b75f1d00b99da4c4705b551dde53b633
Justin Hall <hall.justin.d@gmail.com>
2018-06-13 19:50:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nameToAttributes(drivers){
   
   return drivers.map(function (driver){
     
-    const driverFirstName = driver.split(' ', [0]);
+    const driverFirstName = driver.split(' '), [0];
     const driverLastName = driver.split(' ', [0]);
     
     return {firstName: driverFirstName, lastName: driverLastName};
